c9011924e5f9f01226b0aab9a3b5f5d92a6b3355
Calvin Bauer <nivlac12@WIN2.CS.KSU.EDU>
2019-04-19 14:50:10 -0500

    Created basis for creating table classes in C#

---
+++ Restaurants_Database_UI/Restaurants Database/Restaurants Database/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants_Database
+{
+    class Restaurant
+    {
+        public int RestaurantID { get; }
+        public int OrganizationID { get; }
+        public string RestaurantName { get; }
+        public string DateFounded { get; }
+        public bool IsOperational { get; }
+
+        public Restaurant(int restID, int orgID, string restName, string date, bool isop)
+        {
+            RestaurantID = RestaurantID;
+            OrganizationID = orgID;
+            RestaurantName = restName;
+            DateFounded = date;
+            IsOperational = isop;
+        }
+    }
+}
