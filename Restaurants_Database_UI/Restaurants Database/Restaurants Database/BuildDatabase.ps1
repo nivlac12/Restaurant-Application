52721e3461b051d2f0bfc239bf3cdf05d2bb6d9c
Calvin Bauer <nivlac12@WIN2.CS.KSU.EDU>
2019-04-22 10:43:58 -0500

    Powershell updates

--- Restaurants_Database_UI/Restaurants Database/Restaurants Database/BuildDatabase.ps1
+++ Restaurants_Database_UI/Restaurants Database/Restaurants Database/BuildDatabase.ps1
@@ -1,7 +1,6 @@
 ﻿Param(
    [string] $Server = "(localdb)\MSSQLLocalDb",
    [string] $Database = "nivlac12"
-   [string] $world = "WERE ERW EIJOPA\n\n\n"
 )
 
 Write-Host ""
@@ -12,6 +11,4 @@ Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Suppli
 Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Insert_Suppliers.sql"
 Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Organization.sql"
 Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Insert_Organization.sql"
-Start-Sleep -s 3
-Echo $world
-Start-Sleep -s 3
+Write-Host "Done"
