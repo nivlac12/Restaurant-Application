b33257c3ddd537e6d5b3d7aba7df7e7eca6cc620
Calvin Bauer <nivlac12@WIN2.CS.KSU.EDU>
2019-04-24 14:37:04 -0500

    Added a base controller class

---
+++ Restaurants_Database_UI/Restaurants Database/Restaurants Database/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants_Database
+{
+    class Controller
+    {
+        OrganizationRepo _org;
+        RestaurantRepository _rest;
+        FoodRepo _food;
+        JobsRepo _jobs;
+        StockItemsRepo _stock;
+        EmployeeRepo _emp;
+        SuppliersRepo _suppliers;
+
+        public Controller()
+        {
+            _org = new OrganizationRepo();
+            _rest = new RestaurantRepository();
+            _food = new FoodRepo(); 
+        }
+    
+    }
+}
